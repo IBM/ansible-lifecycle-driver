942bead21044a7b01dd3dfd20a00c19c6c6d24b4
Steve Glover <steve.glover@accantosystems.com>
2019-11-20 10:33:05 +0000

    issue/30: add support for K8s certificate authentication in K8s deployment locations, fix driver version

--- ansibledriver/model/kubeconfig.py
+++ ansibledriver/model/kubeconfig.py
@@ -1,16 +1,59 @@
 import yaml
 
+K8S_SERVER_PROP = 'k8s-server'
+K8S_USERNAME_PROP = 'k8s-username'
+K8S_CERT_AUTH_DATA_PROP = 'k8s-certificate-authority-data'
+K8S_CLIENT_CERT_DATA_PROP = 'k8s-client-certificate-data'
+K8S_CLIENT_KEY_DATA_PROP = 'k8s-client-key-data'
+K8S_TOKEN_PROP = 'k8s-token'
+K8S_NAMESPACE_PROP = "k8s-namespace"
+REGISTRY_URI_PROP = 'registry_uri'
+
 class KubeConfig():
-    def __init__(self, name, k8sServer, k8sToken):
-        self.name = name
+    def __init__(self, deployment_location):
+        self.name = deployment_location.get('name', None)
+        if self.name is None:
+            raise ValueError('Must specify a name for the deployment location')
+
+        dl_properties = deployment_location.get('properties', None)
+        if dl_properties is None:
+            raise ValueError('Deployment location has no properties')
+
+        k8sServer = dl_properties.get(K8S_SERVER_PROP, None)
+        if k8sServer is None:
+            raise ValueError('Deployment location properties must contain a value for {0}'.format(K8S_SERVER_PROP))
+
+        k8sToken = dl_properties.get(K8S_TOKEN_PROP, None)
+        certificateAuthorityData = dl_properties.get(K8S_CERT_AUTH_DATA_PROP, None)
+        clientCertificateData = dl_properties.get(K8S_CLIENT_CERT_DATA_PROP, None)
+        clientKeyData = dl_properties.get(K8S_CLIENT_KEY_DATA_PROP, None)
+
+        if certificateAuthorityData is not None and clientCertificateData is not None and clientKeyData is not None:
+            cluster = {
+                "insecure-skip-tls-verify": False,
+                "server": k8sServer,
+                "certificate-authority-data": certificateAuthorityData
+            }
+            user = {
+                "client-certificate-data": clientCertificateData,
+                "client-key-data": clientKeyData
+            }
+        elif k8sToken is not None:
+            cluster = {
+                "insecure-skip-tls-verify": True,
+                "server": k8sServer
+            }
+            user = {
+                "token": k8sToken
+            }
+        else:
+            raise ValueError('Must specify either {0}, {1}, {2} or {3}'.format(K8S_CERT_AUTH_DATA_PROP, K8S_CLIENT_CERT_DATA_PROP, K8S_CLIENT_KEY_DATA_PROP, K8S_TOKEN_PROP))
+
         self.kubeConfig = {
             "apiVersion": "v1",
             "clusters": [{
                 "name": "mycluster",
-                "cluster": {
-                    "insecure-skip-tls-verify": True,
-                    "server": k8sServer
-                }
+                "cluster": cluster
             }],
             "contexts": [{
                 "name": "mycluster-context",
@@ -24,9 +67,7 @@ class KubeConfig():
             "preferences": {},
             "users": [{
                 "name": "ald-user",
-                "user": {
-                    "token": k8sToken
-                }
+                "user": user
             }]
         }
 
---
+++ tests/unit/service/test_kubeconfig.py
@@ -0,0 +1,102 @@
+import logging
+import unittest
+import time
+import uuid
+import shutil
+import os
+import sys
+from unittest.mock import patch, MagicMock, ANY
+from ansibledriver.model.kubeconfig import KubeConfig
+from ansibledriver.model.kubeconfig import K8S_SERVER_PROP, K8S_TOKEN_PROP, K8S_CERT_AUTH_DATA_PROP, K8S_CLIENT_CERT_DATA_PROP, K8S_CLIENT_KEY_DATA_PROP
+
+logger = logging.getLogger()
+logger.level = logging.INFO
+
+class TestKubeConfig(unittest.TestCase):
+
+    def test_kube_config_with_token(self):
+        deployment_location = {
+            'name': 'dl1',
+            'properties': {
+                K8S_SERVER_PROP: "http://localhost:8080",
+                K8S_TOKEN_PROP: "token"
+            }
+        }
+        c = KubeConfig(deployment_location)
+        self.assertEqual(c.kubeConfig, {
+            "apiVersion": "v1",
+            "clusters": [{
+                "name": "mycluster",
+                "cluster": {
+                    "insecure-skip-tls-verify": True,
+                    "server": "http://localhost:8080"
+                }
+            }],
+            "contexts": [{
+                "name": "mycluster-context",
+                "context": {
+                    "cluster": "mycluster",
+                    "user": "ald-user"
+                }
+            }],
+            "current-context": "mycluster-context",
+            "kind": "Config",
+            "preferences": {},
+            "users": [{
+                "name": "ald-user",
+                "user": {
+                    "token": "token"
+                }
+            }]
+        })
+
+    def test_kube_config_with_certificate(self):
+        deployment_location = {
+            'name': 'dl1',
+            'properties': {
+                K8S_SERVER_PROP: "http://localhost:8080",
+                K8S_CERT_AUTH_DATA_PROP: "cert_auth",
+                K8S_CLIENT_CERT_DATA_PROP: "client_cert",
+                K8S_CLIENT_KEY_DATA_PROP: "client_key"
+            }
+        }
+        c = KubeConfig(deployment_location)
+        self.assertEqual(c.kubeConfig, {
+            "apiVersion": "v1",
+            "clusters": [{
+                "name": "mycluster",
+                "cluster": {
+                    "insecure-skip-tls-verify": False,
+                    "server": "http://localhost:8080",
+                    "certificate-authority-data": "cert_auth"
+                }
+            }],
+            "contexts": [{
+                "name": "mycluster-context",
+                "context": {
+                    "cluster": "mycluster",
+                    "user": "ald-user"
+                }
+            }],
+            "current-context": "mycluster-context",
+            "kind": "Config",
+            "preferences": {},
+            "users": [{
+                "name": "ald-user",
+                "user": {
+                    "client-certificate-data": "client_cert",
+                    "client-key-data": "client_key"
+                }
+            }]
+        })
+
+    def test_kube_config_missing_credentials(self):
+        deployment_location = {
+            'name': 'dl1',
+            'properties': {
+                K8S_SERVER_PROP: "123"
+            }
+        }
+        with self.assertRaises(ValueError) as exc:
+            KubeConfig(deployment_location)
+            self.assertEqual(exc.message, "Must specify either {0}, {1}, {2} or {3}".format(K8S_CERT_AUTH_DATA_PROP, K8S_CLIENT_CERT_DATA_PROP, K8S_CLIENT_KEY_DATA_PROP, K8S_TOKEN_PROP))
