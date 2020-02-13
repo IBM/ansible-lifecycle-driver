e413bafb468d5337c2bb2b325bc9d87bb3222e78
Steve Glover <steve.glover@accantosystems.com>
2020-02-13 11:49:26 +0000

    Fix logging for connexion and ignition service management to INFO for now

--- ansibledriver/__init__.py
+++ ansibledriver/__init__.py
@@ -1,6 +1,11 @@
+import logging
 from ansibledriver.app import create_app
 
 def create_wsgi_app():
     ignition_app = create_app()
+
+    logging.getLogger("connexion").setLevel("INFO")
+    logging.getLogger("ignition.service.management").setLevel("INFO")
+
     # For wsgi deployments
     return ignition_app.connexion_app
