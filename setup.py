e55ec8f92f1495599858626c4478b20e2b50e306
Swati Jaiswal <Swati.Jaiswal@ibm.com>
2021-08-12 14:16:54 +0100

    Fix dependencies from ignition, update ansible to 2.9.13

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'ignition-framework{0}'.format(_pkg_info['ignition-version']),
-        'ansible==2.9.12',
+        'ansible==2.9.13',
         'uwsgi==2.0.19.1',
         'gunicorn==20.1.0',
         'testfixtures==6.17.1',
