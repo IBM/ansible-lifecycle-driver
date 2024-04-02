87a8963e6f56a480c07b08ebe9e79836d9fa0cba
Dave Tobin <davetobi@ie.ibm.com>
2024-04-02 13:56:19 +0100

    Testing

--- build.py
+++ build.py
@@ -102,7 +102,6 @@ class Builder:
         self.stages = []
         self.project_version = None
         self.py_normalized_version = None
-        self.helm_normalized_version = None
 
     def report(self):
         print('================================================')
@@ -214,11 +213,8 @@ class Builder:
                 self.py_normalized_version = pkg_info_data['version']
                 self.py_normalized_version = self.py_normalized_version.replace('-alpha-', 'a')
                 self.py_normalized_version = self.py_normalized_version.replace('-beta-', 'b')
-                self.py_normalized_version = self.py_normalized_version.replace('-rc', 'rc')
-                self.helm_normalized_version = self.py_normalized_version.replace('a', '-alpha.')
-                self.helm_normalized_version = self.py_normalized_version.replace('b', '-beta.')
-                self.helm_normalized_version = self.py_normalized_version.replace('rc', '-rc.')
-                self.helm_normalized_version = self.py_normalized_version.replace('.dev', '-dev.')
+                self.py_normalized_version = self.py_normalized_version.replace('-rc-', 'rc')
+                self.py_normalized_version = self.py_normalized_version.replace('-dev-', '.dev0')
 
     def run_unit_tests(self):
         with self.stage('Run Unit Tests') as s:
@@ -266,7 +262,7 @@ class Builder:
             helm_chart_path = os.path.join(self.project_path, HELM_CHART_PATH)
             template_loader = jinja.FileSystemLoader(searchpath=helm_chart_path)
             template_env = jinja.Environment(variable_start_string='${', variable_end_string='}', loader=template_loader)
-            resolvable_props = {'version': self.helm_normalized_version}
+            resolvable_props = {'version': self.project_version}
             for item in os.listdir(helm_chart_path):
                 full_item_path = os.path.join(helm_chart_path, item)
                 if os.path.isdir(full_item_path):
