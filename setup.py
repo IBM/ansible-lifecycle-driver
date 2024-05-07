aec51798709a099ba1fa58a1b108da20ea266405
Suman Savanur <sumansavanur@in.ibm.com>
2024-05-07 16:20:56 +0530

    Uplift ansible to 9.5.1 version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'ignition-framework=={0}'.format(_pkg_info['ignition-version']),
-        'ansible==4.2.0',
+        'ansible==9.5.1',
         'gunicorn==20.1.0',
         'testfixtures==6.17.1',
         'openstacksdk==0.57.0',
